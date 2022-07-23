93b79b5dc2e4b6dc15d363c6788a4c54bcf7482a
smiley <codemasher@users.noreply.github.com>
2022-07-23 22:59:22 +0200

    :sparkles: QRMatrixTest

---
+++ test/QRCode.test.js
@@ -0,0 +1,32 @@
+/**
+ * @created      23.07.2022
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2022 smiley
+ * @license      MIT
+ */
+
+import {QRCode, QROptions} from '../lib/index.js';
+
+import {suite, test} from 'mocha';
+import {assert} from 'chai';
+
+/**
+ * Tests basic functions of the QRCode class
+ */
+suite('QRCodeTest', function(){
+
+	/**
+	 * tests if an exception is thrown when an invalid (built-in) output type is specified
+	 */
+	test('testInitOutputInterfaceException', function(){
+		assert.throws(() => (new QRCode(new QROptions({outputType: 'foo'}))).render('test'));
+	});
+
+	/**
+	 * tests if an exception is thrown when trying to call getMatrix() without data (empty string, no data set)
+	 */
+	test('testGetMatrixException', function(){
+		assert.throws(() => (new QRCode()).getMatrix());
+	});
+
+});
